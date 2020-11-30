19ef3daef671809005a5f8c0188d7766a60aa12d
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2020-11-29 18:51:32 -0700

    created sql db and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wild Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+
+Select * from Burgers;
